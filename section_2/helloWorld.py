a8aad101456ab8a3b3bef6376a99779e6b5989d0
Andru Estes <andru@linuxacademy.com>
2018-04-20 11:24:50 -0500

    Updated the python code for section 2, live activity 1.

--- section_2/helloWorld.py
+++ section_2/helloWorld.py
@@ -4,7 +4,11 @@ print('Loading your function')
 
 def lambda_handler(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
+    # print statements actually get printed to the logs.
     print("message --> " + event['message'])
 
-    return event['message']  # Echo back the first key value
+    # Actually returning the value of the 'message' key.
+    return event['message']
+
+    # Raising an exception if something goes wrong...
     raise Exception('Something went wrong!')
