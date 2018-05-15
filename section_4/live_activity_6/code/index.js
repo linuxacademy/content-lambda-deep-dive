dbd26a95bfeaf9edeb39348c1e2788359b1894e0
Andru Estes <andru@linuxacademy.com>
2018-05-15 10:05:07 -0500

    Providing code for live act.

---
+++ section_4/live_activity_6/code/index.js
@@ -0,0 +1,126 @@
+// dependencies
+var async = require('async');
+var AWS = require('aws-sdk');
+var gm = require('gm')
+            .subClass({ imageMagick: true }); // Enable ImageMagick integration.
+var util = require('util');
+
+// constants
+var MAX_WIDTH  = 150;
+var MAX_HEIGHT = 150;
+
+// get reference to S3 client
+var s3 = new AWS.S3();
+
+// get reference to SNS client
+var sns = new AWS.SNS();
+
+exports.handler = function(event, context, callback) {
+    // Read options from the event.
+    console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
+    var srcBucket = event.Records[0].s3.bucket.name;
+
+    // Setting value for publishing to SNS
+    var response = event.Records[0].s3.object.key;
+
+    // Object key may have spaces or unicode non-ASCII characters.
+    var srcKey    =
+    decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+
+    // YOU NEED TO EDIT THIS LINE!!!!
+    var dstBucket = "BUCKET";
+    var dstKey    = "resized-" + srcKey;
+
+    // Sanity check: validate that source and destination are different buckets.
+    if (srcBucket == dstBucket) {
+        callback("Source and destination buckets are the same.");
+        return;
+    }
+
+    // Infer the image type.
+    var typeMatch = srcKey.match(/\.([^.]*)$/);
+    if (!typeMatch) {
+        callback("Could not determine the image type.");
+        return;
+    }
+    var imageType = typeMatch[1];
+    if (imageType != "jpg" && imageType != "png") {
+        callback('Unsupported image type: ${imageType}');
+        return;
+    }
+
+    // Download the image from S3, transform, and upload to a different S3 bucket.
+    async.waterfall([
+        function download(next) {
+            // Download the image from S3 into a buffer.
+            s3.getObject({
+                    Bucket: srcBucket,
+                    Key: srcKey
+                },
+                next);
+            },
+        function transform(response, next) {
+            gm(response.Body).size(function(err, size) {
+                // Infer the scaling factor to avoid stretching the image unnaturally.
+                var scalingFactor = Math.min(
+                    MAX_WIDTH / size.width,
+                    MAX_HEIGHT / size.height
+                );
+                var width  = scalingFactor * size.width;
+                var height = scalingFactor * size.height;
+
+                // Transform the image buffer in memory.
+                this.resize(width, height)
+                    .toBuffer(imageType, function(err, buffer) {
+                        if (err) {
+                            next(err);
+                        } else {
+                            next(null, response.ContentType, buffer);
+                        }
+                    });
+            });
+        },
+        function upload(contentType, data, next) {
+            // Stream the transformed image to a different S3 bucket.
+            s3.putObject({
+                    Bucket: dstBucket,
+                    Key: dstKey,
+                    Body: data,
+                    ContentType: contentType
+                },
+                next);
+            }
+        ], function (err) {
+            if (err) {
+                console.error(
+                    'Unable to resize ' + srcBucket + '/' + srcKey +
+                    ' and upload to ' + dstBucket + '/' + dstKey +
+                    ' due to an error: ' + err
+                );
+            } else {
+                console.log(
+                    'Successfully resized ' + srcBucket + '/' + srcKey +
+                    ' and uploaded to ' + dstBucket + '/' + dstKey
+                );
+            }
+
+            // callback(null, "message");
+            callback(null, response);
+
+        }
+    );
+
+    // Sending the object to SNS Topic
+    sns.publish({
+        Message: response,
+        TopicArn: 'ARN GOES HERE!',
+    }, function(err, data) {
+        if (err) {
+            console.log(err.stack);
+            return;
+        }
+        console.log('SNS sent!');
+        console.log(data);
+        context.done(null, 'We are all done.');
+    });
+};
