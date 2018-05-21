f6c64fa11b9bb3ce605891ac0d4cd906094f4035
Andru Estes <andru@linuxacademy.com>
2018-05-21 15:15:40 -0500

    Code for live act.  Creating event source from CLI.

---
+++ section_4/live_activity_6/lambda_function.py
@@ -0,0 +1,46 @@
+import json
+import datetime
+import time
+import boto3
+
+
+## Importing ec2 boto3 client.
+ec2 = boto3.client('ec2')
+
+## Importing SNS client.
+sns = boto3.client('sns')
+
+print('     !Running your function!     ')
+
+
+def lambda_handler(event, context):
+    #print(event)
+
+    ## Getting the Instance ID of the instance that is shutting down.
+    instanceID = event['detail']['instance-id']
+
+    instances = ec2.describe_instances(
+        InstanceIds=[
+            instanceID,
+        ]
+    )
+
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            for device in instance['BlockDeviceMappings']:
+                ebs = device['Ebs']['VolumeId']
+                print("We are taking a snapshot of volume -- %s" % ebs)
+
+                ec2.create_snapshot(
+                    Description='From instance: %s' % instanceID,
+                    VolumeId=ebs,
+                )
+
+                # Sending admin email.
+                sns.publish(
+                    ## EDIT THE ARN
+                    TopicArn='ARN',
+                    Message='Your rule has been triggered to create a snapshot for the following volume -- Volume ID: ' + ebs,
+                    Subject='Snapshot Occured'
+                )
+
---
+++ section_4/live_activity_6/test_event.js
@@ -0,0 +1,16 @@
+{
+  "version": "0",
+  "id": "5d91e795-8bff-41dc-b757-54f252bc37bd",
+  "detail-type": "EC2 Instance State-change Notification",
+  "source": "aws.ec2",
+  "account": "123456789012",
+  "time": "2015-11-11T21:32:26Z",
+  "region": "us-east-1",
+  "resources": [
+    "arn:aws:ec2:us-east-1:123456789012:instance/i-abcd2222"
+  ],
+  "detail": {
+    "instance-id": "i-abcd2222",
+    "state": "stopping"
+  }
+}
