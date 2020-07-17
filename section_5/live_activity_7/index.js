b8b39a1c8137f97cd6034ae54c9b3b40e5377f47
Russ <40004990+rls-vault@users.noreply.github.com>
2020-07-17 19:38:42 +1200

    Update for Node12,  Section 5 Activity 7 (#3)
    * Update for Node12,  Section 5 Activity 7
    
    * Update for Node12,  Section 5 Activity 7

--- section_5/live_activity_7/index.js
+++ section_5/live_activity_7/index.js
@@ -1,9 +1,12 @@
 // dependencies
 var async = require('async');
 var AWS = require('aws-sdk');
-var gm = require('gm')
-            .subClass({ imageMagick: true }); // Enable ImageMagick integration.
+/*var gm = require('gm')
+            .subClass({ imageMagick: true }); // Enable ImageMagick integration.*/
+var sharp = require('sharp'); // Enable Sharp Image manupulaton integration.
 var util = require('util');
+
+// Note: npm install --arch=x64 --platform=linux
 
 // constants
 var MAX_WIDTH  = 150;
@@ -47,36 +50,55 @@ exports.handler = function(event, context, callback) {
     }
 
     // Download the image from S3, transform, and upload to a different S3 bucket.
-    async.waterfall([
-        function download(next) {
-            // Download the image from S3 into a buffer.
-            s3.getObject({
-                    Bucket: srcBucket,
-                    Key: srcKey
-                },
-                next);
-            },
-        function transform(response, next) {
-            gm(response.Body).size(function(err, size) {
-                // Infer the scaling factor to avoid stretching the image unnaturally.
-                var scalingFactor = Math.min(
-                    MAX_WIDTH / size.width,
-                    MAX_HEIGHT / size.height
-                );
-                var width  = scalingFactor * size.width;
-                var height = scalingFactor * size.height;
+  async.waterfall([
+      function download(next) {
+        // Download the image from S3 into a buffer.
+        s3.getObject({
+            Bucket: srcBucket,
+            Key: srcKey
+          },
+          next);
+      },
 
-                // Transform the image buffer in memory.
-                this.resize(width, height)
-                    .toBuffer(imageType, function(err, buffer) {
-                        if (err) {
-                            next(err);
-                        } else {
-                            next(null, response.ContentType, buffer);
-                        }
-                    });
+      // Try sharp .resize with fit:inside  https://sharp.pixelplumbing.com/api-resize
+      function transform(response, next) {
+        sharp(response.Body).resize(MAX_WIDTH, MAX_HEIGHT, {
+          kernel: sharp.kernel.nearest,
+          fit: 'inside',
+          position: 'right top'
+        })
+          .toBuffer(imageType, function (err, buffer) {
+            if (err) {
+              next(err);
+            } else {
+              next(null, response.ContentType, buffer);
+            }
+
+          });
+      },
+
+/*      function transform_old(response, next) {
+        gm(response.Body).size(function (err, size) {
+          // Infer the scaling factor to avoid stretching the image unnaturally.
+          var scalingFactor = Math.min(
+            MAX_WIDTH / size.width,
+            MAX_HEIGHT / size.height
+          );
+          var width = scalingFactor * size.width;
+          var height = scalingFactor * size.height;
+
+          // Transform the image buffer in memory.
+          this.resize(width, height)
+            .toBuffer(imageType, function (err, buffer) {
+              if (err) {
+                next(err);
+              } else {
+                next(null, response.ContentType, buffer);
+              }
             });
-        },
+        });
+      },*/
+
         function upload(contentType, data, next) {
             // Stream the transformed image to a different S3 bucket.
             s3.putObject({
